5237b3454d553bb2e7d549d47ce2efcf0b23c8ad
shamilbin <shamiltk029@gmail.com>
2026-01-23 01:26:30 +0530

    useless

--- app.py
+++ app.py
@@ -1,10 +1,71 @@
-from flask import Flask ,render_template ,url_for
+# from flask import Flask ,render_template ,url_for ,request,redirect
+# from flask_sqlalchemy import SQLAlchemy
+# from datetime import datetime
 
-app=Flask(__name__)
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+# db = SQLAlchemy(app)
 
-@app.route("/")
+# class Todo(db.Model):
+#     id = db.Column(db.Integer , primary_key=True)
+#     content = db.Column(db.String(200) ,nullable=False)
+#     date_created = db.Column(db.DateTime,default =datetime.utcnow)
+
+#     def __repr__(self):
+#         return 'task %r' % self.id
+    
+    
+
+# @app.route("/",methods=['GET','POST'])
+# def index():
+#     if request.method == 'POST':
+#         task_content = request.form['content']
+#         new_Task = Todo(content= task_content)
+#         try:
+#             db.session.add(new_Task)
+#             db.session.commit()
+#             return redirect('/')
+#         except:
+#             return "There was an issue adding your task"
+#     else:
+#          task = Todo.query.order_by(Todo.date_created).all()
+#          return render_template('index.html',tasks=task)
+   
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+    
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_Task = Todo(content=task_content)
+        try:
+            db.session.add(new_Task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding your task"
+    else:
+        task = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=task)
+   
 
 if __name__ == "__main__":
     app.run(debug=True)
