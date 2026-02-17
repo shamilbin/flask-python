ef4b04114bce9d59770915a49aad5935c7bd19b5
shamilbin <shamiltk029@gmail.com>
2026-02-17 22:42:59 +0530

    nothing

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     
+    
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     
 
