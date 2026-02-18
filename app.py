601a380903f94ab3ec6f8823e7949129fbb43889
shamilbin <shamiltk029@gmail.com>
2026-02-18 23:49:32 +0530

    nothing

--- app.py
+++ app.py
@@ -67,6 +67,7 @@ def index():
             return redirect('/')
         except:
             return "There was an issue adding your task"
+        
     else:
         task = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=task)
